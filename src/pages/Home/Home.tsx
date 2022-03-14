34cbf5b2961abd3cce5eb62f6ef064720444c674
jinn2u <bsybear623@gmail.com>
2022-03-14 17:42:31 +0900

    feat: Home 컴포넌트 원상복구

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,7 @@
-import React, { MouseEvent, useState } from "react";
-import FormModal from "src/components/FormModal";
+import React from "react";
+
 const Home = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const handleBtnClick = () => {
-    setModalOpen(true);
-  };
-  const initialMemo = "asdf" as string;
-  return (
-    <div>
-      <button
-        style={{ width: "300px", height: "200px", fontSize: "100px" }}
-        onClick={handleBtnClick}
-      >
-        click
-      </button>
-      {modalOpen && (
-        <FormModal
-          setIsModalOpen={setModalOpen}
-          name="속리산 숲 체험 휴양 마을"
-          address="충청북도 보은군 속리산면 속리산로 596"
-          tel="043-540-3220"
-          initialMemo={initialMemo}
-          handleSubmitBtn={
-            (name, address, tel, memo) => console.log(name, address, tel, memo, "asdf")
-            // 추가로직 작성
-          }
-          handleRemoveBtn={() => console.log("removed")}
-        />
-      )}
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
