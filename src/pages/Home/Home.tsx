83c1e2947b9dcc41f6bb3c45b5f5fe13f6c1d8dc
Parksangwoo <psu8430@gmail.com>
2022-03-14 16:13:11 +0900

    Feat: Home 컴포넌트 세팅 with 더미 데이터

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import SearchBar from '../../components/Search';
+import SavedList from '../../components/SavedList';
+
+import { data } from './data';
+
 
 const Home = () => {
-  return <div>Home</div>;
+  const [filteredMemo, setFilterdMemo] = useState([]);
+
+  useEffect(() => {
+    // 로컬 스토리지에서 데이터 가져오기
+    setFilterdMemo(data);
+  }, [filteredMemo])
+
+  return (
+    <div>
+      <SearchBar filteredMemo={filteredMemo} setFilterdMemo={setFilterdMemo} />
+      <SavedList filteredMemo={filteredMemo} />
+    </div>
+  );
 };
 
 export default Home;
