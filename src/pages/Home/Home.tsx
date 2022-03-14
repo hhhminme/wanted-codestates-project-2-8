8ce1972e20e5bff79ffac9d45fd92e18579fd648
Parksangwoo <psu8430@gmail.com>
2022-03-14 18:18:13 +0900

    Feat: Home 데이터 타입 추가 및 로컬 스토리지 데이터 접근

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,50 @@
 import React, { useEffect, useState } from "react";
 
-import SearchBar from '../../components/Search';
-import SavedList from '../../components/SavedList';
+import Search from "../../components/Search/Search";
+import SavedList from "../../components/SavedList";
 
-import { data } from './data';
+import { data } from "./data";
 
+interface FilterMemo {
+  contents: string;
+  emdNm: string;
+  fcAddr: string;
+  fcAddrDetail: string;
+  fcGbn: string;
+  fcNm: number;
+  fcNo: string;
+  ref1: string;
+  ref2: string;
+  ref3: string;
+  sggNm: string;
+  siNm: string;
+  wDate: string;
+  xp: string;
+  yp: string;
+  zip: string;
+}
+
+interface realType {
+  response: FilterMemo[];
+}
+interface Props {
+  filteredMemo: realType;
+}
 
 const Home = () => {
-  const [filteredMemo, setFilterdMemo] = useState([]);
+  const [filteredMemo, setFilteredMemo] = useState<FilterMemo[]>([]);
 
   useEffect(() => {
     // 로컬 스토리지에서 데이터 가져오기
-    setFilterdMemo(data);
-  }, [filteredMemo])
+    // const localData = JSON.parse(window.localStorage.getItem("key"));
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    setFilteredMemo(data.response as FilterMemo[]);
+    console.log(filteredMemo);
+  }, [filteredMemo]);
 
   return (
     <div>
-      <SearchBar filteredMemo={filteredMemo} setFilterdMemo={setFilterdMemo} />
+      <Search filteredMemo={filteredMemo} setFilteredMemo={setFilteredMemo} />
       <SavedList filteredMemo={filteredMemo} />
     </div>
   );
