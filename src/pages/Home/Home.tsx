af67579485b38b450c5b1f159b668c5245c7df48
jinn2u <bsybear623@gmail.com>
2022-03-14 17:40:04 +0900

    feat: 모달 컴포넌트 제작

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import React, { MouseEvent, useState } from "react";
+import FormModal from "src/components/FormModal";
 const Home = () => {
-  return <div>Home</div>;
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleBtnClick = () => {
+    setModalOpen(true);
+  };
+  const initialMemo = "asdf" as string;
+  return (
+    <div>
+      <button
+        style={{ width: "300px", height: "200px", fontSize: "100px" }}
+        onClick={handleBtnClick}
+      >
+        click
+      </button>
+      {modalOpen && (
+        <FormModal
+          setIsModalOpen={setModalOpen}
+          name="속리산 숲 체험 휴양 마을"
+          address="충청북도 보은군 속리산면 속리산로 596"
+          tel="043-540-3220"
+          initialMemo={initialMemo}
+          handleSubmitBtn={
+            (name, address, tel, memo) => console.log(name, address, tel, memo, "asdf")
+            // 추가로직 작성
+          }
+          handleRemoveBtn={() => console.log("removed")}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Home;
